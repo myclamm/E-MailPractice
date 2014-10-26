c6d5de40ade481bb04018353de4801980bee0da4
Mike Lam <mikelam231@gmail.com>
2014-10-26 16:03:00 -0700

    changed static routes

--- server/config.js
+++ server/config.js
@@ -4,6 +4,6 @@ var path = require('path');
 
 
 module.exports = function(app,express){
-	app.use(express.static(path.join(__dirname,'/app')));
+	app.use(express.static(path.join(__dirname,'./app')));
 	app.use(bodyParser.json());
 }
