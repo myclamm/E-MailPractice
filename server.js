4291ced1803648f40e8663d619bf25e7e6fbb333
Mike Lam <mikelam231@gmail.com>
2014-10-26 11:34:09 -0700

    fixing heroku deploy routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-require('.server/config.js')(app,express);
-require('.server/routes.js')(app);
+require('./server/config.js')(app,express);
+require('./server/routes.js')(app);
 
 var port = process.env.PORT || 3000;
 var url = process.env.URL || 'localhost';
